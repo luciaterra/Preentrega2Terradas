5b86b5bce8611967be9c065cda8fb2aff76877f6
Lucia Terradas <89464830+luciaterra@users.noreply.github.com>
2023-02-22 09:42:16 -0300

    Add files via upload

---
+++ ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ItemListContainer = ({ greeting }) => {
+  const texto = 'Fotografías';
+  return(
+   <ItemList/>
+  );
+};
+
+export default ItemListContainer;
---
+++ itemList.jsx
@@ -0,0 +1,15 @@
+
+
+
+const ItemList = () => {
+    return (
+        <div>
+        <h2>servicios</h2>
+        <p>fotografos</p>
+        <p>contacto</p>
+        <p>adquirí la foto</p>
+      </div>
+    )
+}
+
+export default ItemList
